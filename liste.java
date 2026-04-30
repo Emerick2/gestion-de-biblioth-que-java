24ff796010d0cf46c9c5afb087b2d46cce98ad6d
bkilian <kilian.boudeaud@ynov.com>
2026-04-30 09:25:49 +0200

    gestion liste null

--- liste.java
+++ liste.java
@@ -3,8 +3,12 @@ import bibliotheque;
 
 public class Liste {
     public static void Liste_de_livres(String[] args) {
-        for (int i = 0; i < livre.length; i++) {
-            System.out.println(livre[i].getTitle() + " of " + livre[i].getAuthor());
+        if (String[] == null) {
+            System.out.println("No books to display.");
+        } else {
+            for (int i = 0; i < args.length; i++) {
+                System.out.println(args[i]);
+            }
         }
     }
 }
