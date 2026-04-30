cb488f823953ef4dcf937659e035a5dfd9536f4e
bkilian <kilian.boudeaud@ynov.com>
2026-04-30 08:31:03 +0200

    fonction liste temporaire avant biblio

---
+++ liste.java
@@ -0,0 +1,9 @@
+import livre;
+
+public class liste {
+    public liste-livre[];
+    biblio new liste-livre[100];
+    for (i = 0; i < liste-livre.length; i++) {
+        println(liste-livre[i].titre);
+    }
+}
